39e71c92ee4b5aeebf9684014bbcb4a3bb59dfd9
bismofunyunss <jwynn9690@gmail.com>
2023-06-14 22:03:48 -0400

    .

--- File Protector/Register_Form.cs
+++ File Protector/Register_Form.cs
@@ -53,7 +53,7 @@ namespace File_Protector
             {
                 if (!exists)
                 {
-                    byte[] salt = Crypto.RndByteSized(256 / 8 / 2);
+                    byte[] salt = Crypto.RndByteSized(256 / 8);
                     string hashPassword = Crypto.HashPasswordV2(passTxt.Text, salt);
                     string saltString = Convert.ToBase64String(salt);
 
